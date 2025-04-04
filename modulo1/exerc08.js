e3cf9067ed91d8e420e943a483b02e6cacb3bd25
Juliana Marques <marquesjuliana849@gmail.com>
2025-04-04 10:48:53 -0300

    att

---
+++ modulo1/exerc08.js
@@ -0,0 +1,6 @@
+var vel = 25
+console.log(`A velocidade do carro é de ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Velocidade acima da permitida. MULTADO!`)
+}
+console.log("Dirija com cuidade!")
